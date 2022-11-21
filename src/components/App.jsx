34eec405231f1769c7b7b627862a4b8ba7b174d5
olhazamlynska <olya.sonechko@gmail.com>
2022-11-21 19:33:07 +0200

    Add profile and statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import {Profile } from "./User/User";
+import user from '../data/user.json';
+
+import { Statistics } from "./Statistics/Statistics";
+import data from '../data/data.json';
+
+
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +19,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+
+export const Statistics = ({title, stats}) => {
+   return <section className="statistics">
+  <h2 className="title">{title}</h2>
+      <ul className="stat-list">
+         {stats.map(({id, label,percentage}) => {
+            return <li className="item" key={id}> <span className="label">{label}</span>
+      <span className="percentage">{percentage}%</span></li>
+         })}
+  </ul>
+</section>
+}
+
+Statistics.propTypes = {
+   title: PropTypes.string,
+   stats: PropTypes.arrayOf(PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+   
+  }).isRequired)
+}
